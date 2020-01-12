5b17b2fd3b618fa201bb2fc333fea84d6e78eee7
ofats <ogfatog@gmail.com>
2020-01-12 15:58:18 +0100

    Change formatting rule and apply it.

--- include/ofats/invocable.h
+++ include/ofats/invocable.h
@@ -64,77 +64,73 @@ inline constexpr bool is_small_object_v =
     std::is_nothrow_move_constructible_v<T>;
 
 union storage {
-    void* ptr_ = nullptr;
-    buffer buf_;
+  void* ptr_ = nullptr;
+  buffer buf_;
 };
 
 enum class action { destroy, move };
 
 template <class R, class... ArgTypes>
 struct handler_traits {
-    template <class Derived>
-    struct handler_base {
-        static void handle(action act, storage* current,
-                           storage* other = nullptr) {
-            switch (act) {
-                case (action::destroy):
-                    Derived::destroy(*current);
-                    break;
-                case (action::move):
-                    Derived::move(*current, *other);
-                    break;
-            }
-        }
-    };
+  template <class Derived>
+  struct handler_base {
+    static void handle(action act, storage* current, storage* other = nullptr) {
+      switch (act) {
+        case (action::destroy):
+          Derived::destroy(*current);
+          break;
+        case (action::move):
+          Derived::move(*current, *other);
+          break;
+      }
+    }
+  };
 
-    template <class T>
-    struct small_handler : handler_base<small_handler<T>> {
-        template <class... Args>
-        static void create(storage& s, Args&&... args) {
-            new (static_cast<void*>(&s.buf_)) T(std::forward<Args>(args)...);
-        }
+  template <class T>
+  struct small_handler : handler_base<small_handler<T>> {
+    template <class... Args>
+    static void create(storage& s, Args&&... args) {
+      new (static_cast<void*>(&s.buf_)) T(std::forward<Args>(args)...);
+    }
 
-        static void destroy(storage& s) noexcept {
-            T& value = *static_cast<T*>(static_cast<void*>(&s.buf_));
-            value.~T();
-        }
+    static void destroy(storage& s) noexcept {
+      T& value = *static_cast<T*>(static_cast<void*>(&s.buf_));
+      value.~T();
+    }
 
-        static void move(storage& dst, storage& src) noexcept {
-            create(dst,
-                   std::move(*static_cast<T*>(static_cast<void*>(&src.buf_))));
-            destroy(src);
-        }
+    static void move(storage& dst, storage& src) noexcept {
+      create(dst, std::move(*static_cast<T*>(static_cast<void*>(&src.buf_))));
+      destroy(src);
+    }
 
-        static R call(storage& s, ArgTypes... args) {
-            return std::invoke(*static_cast<T*>(static_cast<void*>(&s.buf_)),
-                               std::forward<ArgTypes>(args)...);
-        }
-    };
+    static R call(storage& s, ArgTypes... args) {
+      return std::invoke(*static_cast<T*>(static_cast<void*>(&s.buf_)),
+                         std::forward<ArgTypes>(args)...);
+    }
+  };
 
-    template <class T>
-    struct large_handler : handler_base<large_handler<T>> {
-        template <class... Args>
-        static void create(storage& s, Args&&... args) {
-            s.ptr_ = new T(std::forward<Args>(args)...);
-        }
+  template <class T>
+  struct large_handler : handler_base<large_handler<T>> {
+    template <class... Args>
+    static void create(storage& s, Args&&... args) {
+      s.ptr_ = new T(std::forward<Args>(args)...);
+    }
 
-        static void destroy(storage& s) noexcept {
-            delete static_cast<T*>(s.ptr_);
-        }
+    static void destroy(storage& s) noexcept { delete static_cast<T*>(s.ptr_); }
 
-        static void move(storage& dst, storage& src) noexcept {
-            dst.ptr_ = src.ptr_;
-        }
+    static void move(storage& dst, storage& src) noexcept {
+      dst.ptr_ = src.ptr_;
+    }
 
-        static R call(storage& s, ArgTypes... args) {
-            return std::invoke(*static_cast<T*>(s.ptr_),
-                               std::forward<ArgTypes>(args)...);
-        }
-    };
+    static R call(storage& s, ArgTypes... args) {
+      return std::invoke(*static_cast<T*>(s.ptr_),
+                         std::forward<ArgTypes>(args)...);
+    }
+  };
 
-    template <class T>
-    using handler = std::conditional_t<is_small_object_v<T>, small_handler<T>,
-                                       large_handler<T>>;
+  template <class T>
+  using handler = std::conditional_t<is_small_object_v<T>, small_handler<T>,
+                                     large_handler<T>>;
 };
 
 template <class T>
@@ -153,156 +149,154 @@ class any_invocable;
 
 template <class R, class... ArgTypes>
 class any_invocable<R(ArgTypes...)> {
-    template <class T>
-    using handler =
-        typename any_detail::handler_traits<R,
-                                            ArgTypes...>::template handler<T>;
+  template <class T>
+  using handler =
+      typename any_detail::handler_traits<R, ArgTypes...>::template handler<T>;
 
-    using storage = any_detail::storage;
-    using action = any_detail::action;
-    using handle_func = void (*)(any_detail::action, any_detail::storage*,
-                                 any_detail::storage*);
-    using call_func = R (*)(any_detail::storage&, ArgTypes...);
+  using storage = any_detail::storage;
+  using action = any_detail::action;
+  using handle_func = void (*)(any_detail::action, any_detail::storage*,
+                               any_detail::storage*);
+  using call_func = R (*)(any_detail::storage&, ArgTypes...);
 
-public:
-    using result_type = R;
+ public:
+  using result_type = R;
 
-public:
-    any_invocable() noexcept = default;
-    any_invocable(std::nullptr_t) noexcept {}
-    any_invocable(any_invocable&& rhs) noexcept {
-        if (rhs.handle_) {
-            handle_ = rhs.handle_;
-            handle_(action::move, &storage_, &rhs.storage_);
-            call_ = rhs.call_;
-            rhs.handle_ = nullptr;
-        }
+ public:
+  any_invocable() noexcept = default;
+  any_invocable(std::nullptr_t) noexcept {}
+  any_invocable(any_invocable&& rhs) noexcept {
+    if (rhs.handle_) {
+      handle_ = rhs.handle_;
+      handle_(action::move, &storage_, &rhs.storage_);
+      call_ = rhs.call_;
+      rhs.handle_ = nullptr;
     }
-    template <
-        class F, class FDec = std::decay_t<F>,
-        class = std::enable_if_t<!std::is_same_v<FDec, any_invocable> &&
-                                 !any_detail::is_in_place_type_v<FDec> &&
-                                 std::is_constructible_v<FDec, F> &&
-                                 std::is_move_constructible_v<FDec> &&
-                                 std::is_invocable_r_v<R, FDec, ArgTypes...>>>
-    any_invocable(F&& f) {
-        create<FDec>(std::forward<F>(f));
-    }
+  }
+  template <
+      class F, class FDec = std::decay_t<F>,
+      class = std::enable_if_t<!std::is_same_v<FDec, any_invocable> &&
+                               !any_detail::is_in_place_type_v<FDec> &&
+                               std::is_constructible_v<FDec, F> &&
+                               std::is_move_constructible_v<FDec> &&
+                               std::is_invocable_r_v<R, FDec, ArgTypes...>>>
+  any_invocable(F&& f) {
+    create<FDec>(std::forward<F>(f));
+  }
 
-    template <
-        class T, class... Args, class VT = std::decay_t<T>,
-        class = std::enable_if_t<std::is_move_constructible_v<VT> &&
-                                 std::is_constructible_v<VT, Args...> &&
-                                 std::is_invocable_r_v<R, VT, ArgTypes...>>>
-    explicit any_invocable(std::in_place_type_t<T>, Args&&... args) {
-        create<VT>(std::forward<Args>(args)...);
-    }
+  template <class T, class... Args, class VT = std::decay_t<T>,
+            class = std::enable_if_t<std::is_move_constructible_v<VT> &&
+                                     std::is_constructible_v<VT, Args...> &&
+                                     std::is_invocable_r_v<R, VT, ArgTypes...>>>
+  explicit any_invocable(std::in_place_type_t<T>, Args&&... args) {
+    create<VT>(std::forward<Args>(args)...);
+  }
 
-    template <
-        class T, class U, class... Args, class VT = std::decay_t<T>,
-        class = std::enable_if_t<
-            std::is_move_constructible_v<VT> &&
-            std::is_constructible_v<VT, std::initializer_list<U>&, Args...> &&
-            std::is_invocable_r_v<R, VT, ArgTypes...>>>
-    explicit any_invocable(std::in_place_type_t<T>, std::initializer_list<U> il,
-                           Args&&... args) {
-        create<VT>(il, std::forward<Args>(args)...);
-    }
+  template <
+      class T, class U, class... Args, class VT = std::decay_t<T>,
+      class = std::enable_if_t<
+          std::is_move_constructible_v<VT> &&
+          std::is_constructible_v<VT, std::initializer_list<U>&, Args...> &&
+          std::is_invocable_r_v<R, VT, ArgTypes...>>>
+  explicit any_invocable(std::in_place_type_t<T>, std::initializer_list<U> il,
+                         Args&&... args) {
+    create<VT>(il, std::forward<Args>(args)...);
+  }
 
-    any_invocable& operator=(any_invocable&& rhs) noexcept {
-        any_invocable{std::move(rhs)}.swap(*this);
-        return *this;
-    }
-    any_invocable& operator=(std::nullptr_t) noexcept {
-        destroy();
-        return *this;
-    }
-    template <class F, class FDec = std::decay_t<F>>
-    std::enable_if_t<!std::is_same_v<FDec, any_invocable> &&
-                         std::is_move_constructible_v<FDec>,
-                     any_invocable&>
-    operator=(F&& f) {
-        any_invocable{std::forward<F>(f)}.swap(*this);
-        return *this;
-    }
-    template <class F>
-    any_invocable& operator=(std::reference_wrapper<F> f) {
-        any_invocable{f}.swap(*this);
-        return *this;
-    }
+  any_invocable& operator=(any_invocable&& rhs) noexcept {
+    any_invocable{std::move(rhs)}.swap(*this);
+    return *this;
+  }
+  any_invocable& operator=(std::nullptr_t) noexcept {
+    destroy();
+    return *this;
+  }
+  template <class F, class FDec = std::decay_t<F>>
+  std::enable_if_t<!std::is_same_v<FDec, any_invocable> &&
+                       std::is_move_constructible_v<FDec>,
+                   any_invocable&>
+  operator=(F&& f) {
+    any_invocable{std::forward<F>(f)}.swap(*this);
+    return *this;
+  }
+  template <class F>
+  any_invocable& operator=(std::reference_wrapper<F> f) {
+    any_invocable{f}.swap(*this);
+    return *this;
+  }
 
-    ~any_invocable() { destroy(); }
+  ~any_invocable() { destroy(); }
 
-    void swap(any_invocable& rhs) noexcept {
-        if (handle_) {
-            if (rhs.handle_) {
-                storage tmp;
-                handle_(action::move, &tmp, &storage_);
-                rhs.handle_(action::move, &storage_, &rhs.storage_);
-                handle_(action::move, &rhs.storage_, &tmp);
-                std::swap(handle_, rhs.handle_);
-                std::swap(call_, rhs.call_);
-            } else {
-                rhs.swap(*this);
-            }
-        } else if (rhs.handle_) {
-            rhs.handle_(action::move, &storage_, &rhs.storage_);
-            handle_ = rhs.handle_;
-            call_ = rhs.call_;
-            rhs.handle_ = nullptr;
-        }
+  void swap(any_invocable& rhs) noexcept {
+    if (handle_) {
+      if (rhs.handle_) {
+        storage tmp;
+        handle_(action::move, &tmp, &storage_);
+        rhs.handle_(action::move, &storage_, &rhs.storage_);
+        handle_(action::move, &rhs.storage_, &tmp);
+        std::swap(handle_, rhs.handle_);
+        std::swap(call_, rhs.call_);
+      } else {
+        rhs.swap(*this);
+      }
+    } else if (rhs.handle_) {
+      rhs.handle_(action::move, &storage_, &rhs.storage_);
+      handle_ = rhs.handle_;
+      call_ = rhs.call_;
+      rhs.handle_ = nullptr;
     }
+  }
 
-    explicit operator bool() const noexcept { return handle_ != nullptr; }
+  explicit operator bool() const noexcept { return handle_ != nullptr; }
 
-    R operator()(ArgTypes... args) {
-        return call_(storage_, std::forward<ArgTypes>(args)...);
+  R operator()(ArgTypes... args) {
+    return call_(storage_, std::forward<ArgTypes>(args)...);
+  }
+
+ private:
+  template <class F, class... Args>
+  void create(Args&&... args) {
+    using hdl = handler<F>;
+    hdl::create(storage_, std::forward<Args>(args)...);
+    handle_ = &hdl::handle;
+    call_ = &hdl::call;
+  }
+
+  void destroy() noexcept {
+    if (handle_) {
+      handle_(action::destroy, &storage_, nullptr);
+      handle_ = nullptr;
     }
+  }
 
-private:
-    template <class F, class... Args>
-    void create(Args&&... args) {
-        using hdl = handler<F>;
-        hdl::create(storage_, std::forward<Args>(args)...);
-        handle_ = &hdl::handle;
-        call_ = &hdl::call;
-    }
-
-    void destroy() noexcept {
-        if (handle_) {
-            handle_(action::destroy, &storage_, nullptr);
-            handle_ = nullptr;
-        }
-    }
-
-private:
-    storage storage_;
-    handle_func handle_ = nullptr;
-    call_func call_;
+ private:
+  storage storage_;
+  handle_func handle_ = nullptr;
+  call_func call_;
 };
 
 template <class R, class... ArgTypes>
 bool operator==(const any_invocable<R(ArgTypes...)>& f,
                 std::nullptr_t) noexcept {
-    return !f;
+  return !f;
 }
 
 template <class R, class... ArgTypes>
 bool operator==(std::nullptr_t,
                 const any_invocable<R(ArgTypes...)>& f) noexcept {
-    return !f;
+  return !f;
 }
 
 template <class R, class... ArgTypes>
 bool operator!=(const any_invocable<R(ArgTypes...)>& f,
                 std::nullptr_t) noexcept {
-    return static_cast<bool>(f);
+  return static_cast<bool>(f);
 }
 
 template <class R, class... ArgTypes>
 bool operator!=(std::nullptr_t,
                 const any_invocable<R(ArgTypes...)>& f) noexcept {
-    return static_cast<bool>(f);
+  return static_cast<bool>(f);
 }
 
 }  // namespace ofats
@@ -312,7 +306,7 @@ namespace std {
 template <class R, class... ArgTypes>
 void swap(ofats::any_invocable<R(ArgTypes...)>& lhs,
           ofats::any_invocable<R(ArgTypes...)>& rhs) noexcept {
-    lhs.swap(rhs);
+  lhs.swap(rhs);
 }
 
 }  // namespace std
--- test/invocable_ut.cpp
+++ test/invocable_ut.cpp
@@ -1,224 +1,223 @@
-#include "invocable.h"
-
-#include "catch2/catch.hpp"
-
 #include <array>
 #include <numeric>
+
+#include "catch2/catch.hpp"
+#include "invocable.h"
 
 namespace {
 
 int foo() { return 5; }
 
 struct widget {
-    int boo() { return foo(); }
+  int boo() { return foo(); }
 };
 
 struct statefull_widget {
-    statefull_widget(int val) : value(val) {}
-    statefull_widget(std::initializer_list<int> il)
-        : value(std::accumulate(il.begin(), il.end(), 0)) {}
-    int operator()() { return value; }
-    int value = 0;
+  statefull_widget(int val) : value(val) {}
+  statefull_widget(std::initializer_list<int> il)
+      : value(std::accumulate(il.begin(), il.end(), 0)) {}
+  int operator()() { return value; }
+  int value = 0;
 };
 
 template <class R, class... ArgTypes>
 void require_empty(const ofats::any_invocable<R(ArgTypes...)>& f) {
-    REQUIRE(!f);
-    REQUIRE(f == nullptr);
-    REQUIRE(nullptr == f);
+  REQUIRE(!f);
+  REQUIRE(f == nullptr);
+  REQUIRE(nullptr == f);
 }
 
 template <class R, class... ArgTypes>
 void require_non_empty(const ofats::any_invocable<R(ArgTypes...)>& f) {
-    REQUIRE(f);
-    REQUIRE(f != nullptr);
-    REQUIRE(nullptr != f);
+  REQUIRE(f);
+  REQUIRE(f != nullptr);
+  REQUIRE(nullptr != f);
 }
 
 }  // namespace
 
 TEST_CASE("Construction", "[any_invokable]") {
-    using inv_type = ofats::any_invocable<int()>;
+  using inv_type = ofats::any_invocable<int()>;
 
-    SECTION("Default construction") {
-        auto f = inv_type{};
-        require_empty(f);
-    }
+  SECTION("Default construction") {
+    auto f = inv_type{};
+    require_empty(f);
+  }
 
-    SECTION("Move construction") {
-        auto f = inv_type{foo};
-        require_non_empty(f);
-        REQUIRE(f() == 5);
-        auto f2 = inv_type{std::move(f)};
-        require_empty(f);
-        require_non_empty(f2);
-        REQUIRE(f2() == 5);
-    }
+  SECTION("Move construction") {
+    auto f = inv_type{foo};
+    require_non_empty(f);
+    REQUIRE(f() == 5);
+    auto f2 = inv_type{std::move(f)};
+    require_empty(f);
+    require_non_empty(f2);
+    REQUIRE(f2() == 5);
+  }
 
-    SECTION("Construction from nullptr") {
-        auto f = inv_type{nullptr};
-        require_empty(f);
-    }
+  SECTION("Construction from nullptr") {
+    auto f = inv_type{nullptr};
+    require_empty(f);
+  }
 
-    SECTION("Construction from function") {
-        auto f = inv_type{foo};
-        require_non_empty(f);
-        REQUIRE(f() == 5);
-    }
+  SECTION("Construction from function") {
+    auto f = inv_type{foo};
+    require_non_empty(f);
+    REQUIRE(f() == 5);
+  }
 
-    SECTION("Construction from lambda expression") {
-        auto f = inv_type{[] { return foo(); }};
-        require_non_empty(f);
-        REQUIRE(f() == foo());
-    }
+  SECTION("Construction from lambda expression") {
+    auto f = inv_type{[] { return foo(); }};
+    require_non_empty(f);
+    REQUIRE(f() == foo());
+  }
 
-    SECTION("Construction from member function pointer") {
-        auto f = ofats::any_invocable<int(widget*)>{&widget::boo};
-        widget w;
-        require_non_empty(f);
-        REQUIRE(f(&w) == foo());
-    }
+  SECTION("Construction from member function pointer") {
+    auto f = ofats::any_invocable<int(widget*)>{&widget::boo};
+    widget w;
+    require_non_empty(f);
+    REQUIRE(f(&w) == foo());
+  }
 
-    SECTION("Construction from large object") {
-        auto arr = std::array<int, 32>{};
-        arr.fill(foo());
-        auto f = inv_type{
-            [arr] { return std::accumulate(arr.cbegin(), arr.cend(), 0); }};
-        require_non_empty(f);
-        REQUIRE(f() == std::accumulate(arr.cbegin(), arr.cend(), 0));
-    }
+  SECTION("Construction from large object") {
+    auto arr = std::array<int, 32>{};
+    arr.fill(foo());
+    auto f = inv_type{
+        [arr] { return std::accumulate(arr.cbegin(), arr.cend(), 0); }};
+    require_non_empty(f);
+    REQUIRE(f() == std::accumulate(arr.cbegin(), arr.cend(), 0));
+  }
 
-    SECTION("Construction using in_place_type") {
-        auto f = inv_type{std::in_place_type<statefull_widget>, foo()};
-        require_non_empty(f);
-        REQUIRE(f() == foo());
-    }
+  SECTION("Construction using in_place_type") {
+    auto f = inv_type{std::in_place_type<statefull_widget>, foo()};
+    require_non_empty(f);
+    REQUIRE(f() == foo());
+  }
 
-    SECTION("Construction using in_place_type and initializer_list") {
-        auto f = inv_type{std::in_place_type<statefull_widget>,
-                          {foo(), foo(), foo()}};
-        require_non_empty(f);
-        REQUIRE(f() == foo() * 3);
-    }
+  SECTION("Construction using in_place_type and initializer_list") {
+    auto f =
+        inv_type{std::in_place_type<statefull_widget>, {foo(), foo(), foo()}};
+    require_non_empty(f);
+    REQUIRE(f() == foo() * 3);
+  }
 
-    SECTION("Construction from move-only callable") {
-        auto f = inv_type{[p = std::make_unique<int>(foo())] { return *p; }};
-        require_non_empty(f);
-        REQUIRE(f() == foo());
-    }
+  SECTION("Construction from move-only callable") {
+    auto f = inv_type{[p = std::make_unique<int>(foo())] { return *p; }};
+    require_non_empty(f);
+    REQUIRE(f() == foo());
+  }
 }
 
 TEST_CASE("Assignment", "[any_invocable]") {
-    ofats::any_invocable<int()> f;
+  ofats::any_invocable<int()> f;
+  require_empty(f);
+
+  SECTION("Assignment of default constructed") {
+    f = decltype(f){};
     require_empty(f);
+  }
 
-    SECTION("Assignment of default constructed") {
-        f = decltype(f){};
-        require_empty(f);
-    }
+  SECTION("Move assignment") {
+    auto f2 = decltype(f){foo};
+    require_non_empty(f2);
+    REQUIRE(f2() == foo());
+    f = std::move(f2);
+    require_empty(f2);
+    require_non_empty(f);
+    REQUIRE(f() == foo());
+  }
 
-    SECTION("Move assignment") {
-        auto f2 = decltype(f){foo};
-        require_non_empty(f2);
-        REQUIRE(f2() == foo());
-        f = std::move(f2);
-        require_empty(f2);
-        require_non_empty(f);
-        REQUIRE(f() == foo());
-    }
+  SECTION("Assignment of nullptr") {
+    f = nullptr;
+    require_empty(f);
+  }
 
-    SECTION("Assignment of nullptr") {
-        f = nullptr;
-        require_empty(f);
-    }
+  SECTION("Assignment of function") {
+    f = foo;
+    require_non_empty(f);
+    REQUIRE(f() == 5);
+  }
 
-    SECTION("Assignment of function") {
-        f = foo;
-        require_non_empty(f);
-        REQUIRE(f() == 5);
-    }
+  SECTION("Assignment of lambda expression") {
+    f = [] { return foo(); };
+    require_non_empty(f);
+    REQUIRE(f() == foo());
+  }
 
-    SECTION("Assignment of lambda expression") {
-        f = [] { return foo(); };
-        require_non_empty(f);
-        REQUIRE(f() == foo());
-    }
+  SECTION("Assignment of member function pointer") {
+    ofats::any_invocable<int(widget*)> f;
+    require_empty(f);
+    f = &widget::boo;
+    require_non_empty(f);
+    widget w;
+    REQUIRE(f(&w) == foo());
+  }
 
-    SECTION("Assignment of member function pointer") {
-        ofats::any_invocable<int(widget*)> f;
-        require_empty(f);
-        f = &widget::boo;
-        require_non_empty(f);
-        widget w;
-        REQUIRE(f(&w) == foo());
-    }
+  SECTION("Assignment of reference wrapper") {
+    auto lmb = [] { return foo(); };
+    f = std::ref(lmb);
+    require_non_empty(f);
+    REQUIRE(f() == lmb());
+  }
 
-    SECTION("Assignment of reference wrapper") {
-        auto lmb = [] { return foo(); };
-        f = std::ref(lmb);
-        require_non_empty(f);
-        REQUIRE(f() == lmb());
-    }
-
-    SECTION("Assignment of move-only callable") {
-        f = [p = std::make_unique<int>(foo())] { return *p; };
-        require_non_empty(f);
-        REQUIRE(f() == foo());
-    }
+  SECTION("Assignment of move-only callable") {
+    f = [p = std::make_unique<int>(foo())] { return *p; };
+    require_non_empty(f);
+    REQUIRE(f() == foo());
+  }
 }
 
 TEST_CASE("Swap and comparisons", "[any_invocable]") {
-    using inv_type = ofats::any_invocable<int()>;
-    auto func_inv = inv_type{foo};
-    auto lmb_inv = inv_type{[] { return foo(); }};
-    auto empty_inv1 = inv_type{};
-    auto empty_inv2 = inv_type{};
+  using inv_type = ofats::any_invocable<int()>;
+  auto func_inv = inv_type{foo};
+  auto lmb_inv = inv_type{[] { return foo(); }};
+  auto empty_inv1 = inv_type{};
+  auto empty_inv2 = inv_type{};
 
-    SECTION("Swapping non-empty") {
-        require_non_empty(func_inv);
-        require_non_empty(lmb_inv);
-        REQUIRE(func_inv() == lmb_inv());
-        func_inv.swap(lmb_inv);
+  SECTION("Swapping non-empty") {
+    require_non_empty(func_inv);
+    require_non_empty(lmb_inv);
+    REQUIRE(func_inv() == lmb_inv());
+    func_inv.swap(lmb_inv);
 
-        require_non_empty(func_inv);
-        require_non_empty(lmb_inv);
-        REQUIRE(func_inv() == lmb_inv());
-        lmb_inv.swap(func_inv);
+    require_non_empty(func_inv);
+    require_non_empty(lmb_inv);
+    REQUIRE(func_inv() == lmb_inv());
+    lmb_inv.swap(func_inv);
 
-        require_non_empty(func_inv);
-        require_non_empty(lmb_inv);
-        REQUIRE(func_inv() == lmb_inv());
-    }
+    require_non_empty(func_inv);
+    require_non_empty(lmb_inv);
+    REQUIRE(func_inv() == lmb_inv());
+  }
 
-    SECTION("Swapping empty and non-empty") {
-        require_non_empty(func_inv);
-        REQUIRE(func_inv() == 5);
-        require_empty(empty_inv1);
+  SECTION("Swapping empty and non-empty") {
+    require_non_empty(func_inv);
+    REQUIRE(func_inv() == 5);
+    require_empty(empty_inv1);
 
-        func_inv.swap(empty_inv1);
-        require_non_empty(empty_inv1);
-        REQUIRE(empty_inv1() == 5);
-        require_empty(func_inv);
+    func_inv.swap(empty_inv1);
+    require_non_empty(empty_inv1);
+    REQUIRE(empty_inv1() == 5);
+    require_empty(func_inv);
 
-        require_empty(empty_inv2);
-        empty_inv2.swap(empty_inv1);
-        require_non_empty(empty_inv2);
-        REQUIRE(empty_inv2() == 5);
-        require_empty(empty_inv1);
-    }
+    require_empty(empty_inv2);
+    empty_inv2.swap(empty_inv1);
+    require_non_empty(empty_inv2);
+    REQUIRE(empty_inv2() == 5);
+    require_empty(empty_inv1);
+  }
 
-    SECTION("Swapping empty") {
-        require_empty(empty_inv1);
-        require_empty(empty_inv2);
+  SECTION("Swapping empty") {
+    require_empty(empty_inv1);
+    require_empty(empty_inv2);
 
-        empty_inv1.swap(empty_inv2);
-        require_empty(empty_inv1);
-        require_empty(empty_inv2);
+    empty_inv1.swap(empty_inv2);
+    require_empty(empty_inv1);
+    require_empty(empty_inv2);
 
-        empty_inv2.swap(empty_inv1);
-        require_empty(empty_inv1);
-        require_empty(empty_inv2);
-    }
+    empty_inv2.swap(empty_inv1);
+    require_empty(empty_inv1);
+    require_empty(empty_inv2);
+  }
 }
 
 // TODO(ofats): small vs. large tests, number of move,ctor,dtor test,...
